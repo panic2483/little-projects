c74a2dc54756b83acef485a17e96d8e6ced8d5de
Patrick <patrick.m.fricke@gmx.de>
2025-08-02 16:35:32 +0200

    Add GIF creation script and remove unused team pictures

--- gif/gif.py
+++ gif/gif.py
@@ -1,1 +1,9 @@
 import imageio.v3 as iio
+
+filenames = ["gif/chicklet1.png", "gif/chicklet2.png", "gif/chicklet3.png", "gif/chicklet4.png"]
+images = [ ]
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite("gif/chicken.gif", images, duration=500, loop = 0)
